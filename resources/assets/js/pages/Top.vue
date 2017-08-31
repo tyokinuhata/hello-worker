1744a570f8a2161f2dc1356e19de5a08b6023779
Kazuki Tobita <kazukiti201@gmail.com>
2017-09-01 00:45:29 +0900

    vueファイルの作成とTopページのマークアップ

---
+++ resources/assets/js/pages/Top.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>hello, worker</h1>
+        <a href="">勤怠管理ページ</a>
+        <a href="">管理者用ページ</a>
+    </div>
+</template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,18 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Hello Vue</title>
-    <link rel="stylesheet" href="css/app.css"/>
-    <script type="text/javascript">
-        window.Laravel = window.Laravel || {};
-        window.Laravel.csrfToken = "{{csrf_token()}}";
-    </script>
-</head>
-<body>
-<div id="app">
-    <example></example>
-</div>
-<script src="js/app.js"></script>
-</body>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello Worker</title>
+        <link rel="stylesheet" href="css/app.css"/>
+    </head>
+    <body>
+        <div id="app">
+            <example></example>
+        </div>
+        <script>
+            window.Laravel = window.Laravel || {};
+            window.Laravel.csrfToken = "{{csrf_token()}}";
+        </script>
+        <script src="js/app.js"></script>
+    </body>
 </html>
