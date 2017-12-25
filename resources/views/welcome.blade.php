741e00965b7d85581c5f99d24724a7c8b0d2e60c
Kazuki Tobita <kazukiti201@gmail.com>
2017-12-25 12:54:56 +0900

    トップページ作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,77 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="ja">
     <head>
-        <meta charset="UTF-8">
-        <title>Hello Worker</title>
-        <link rel="stylesheet" href="css/app.css"/>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>hello, worker</title>
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
     </head>
     <body>
-        <h1>hello, worker</h1>
-        <script>
-            window.Laravel = window.Laravel || {};
-            window.Laravel.csrfToken = "{{csrf_token()}}";
-        </script>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    hello, worker
+                </div>
+
+                <div class="links">
+                    <a href="">勤怠管理</a>
+                    <a href="">管理画面</a>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
