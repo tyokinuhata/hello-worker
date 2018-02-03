7f7747f47742391ea134702dbea0fc59afe350db
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-03 17:19:19 +0900

    layouts.app blade修正

--- resources/views/works/top.blade.php
+++ resources/views/works/top.blade.php
@@ -9,7 +9,7 @@
             <label for="remote">リモート</label>
             <button class="btn btn-primary">出勤</button>
             <button class="btn btn-default">退勤</button>
-            <button class="btn btn-warning">一時停止</button>
+            <button class="btn btn-warning">停止</button>
             <button class="btn btn-info">再開</button>
             <button class="btn btn-danger">取消</button>
         </div>
