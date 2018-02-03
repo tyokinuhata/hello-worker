9e753ddc4fe8ed3ff78acd1516337ab6b2782ad4
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-04 00:23:04 +0900

    グローバル変数を定義

--- app/Http/Controllers/WorksController.php
+++ app/Http/Controllers/WorksController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Config;
 
 class WorksController extends Controller {
     public function index() {
+        dd(Config::get('mode'));
         return view('works.top');
     }
 
