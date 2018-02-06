9b509259b858d679a21818e4cd7b58796e6a1ec1
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-07 04:30:45 +0900

    [勤怠管理] 給与明細のマークアップ修正

--- resources/views/works/payslip.blade.php
+++ resources/views/works/payslip.blade.php
@@ -19,6 +19,35 @@
         <div>
             <h2>給与明細</h2>
             <table class="table bg-white">
+                @if(count($payslip) > 0)
+                    <tr>
+                        <th>月日</th>
+                        <th>出勤時間</th>
+                        <th>交通費</th>
+                        <th>出勤形態</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                    <tr>
+                        @foreach($payslip as $row)
+                            <td>{{ $row->date }}</td>
+                            <td>{{ $row->hour }}時間{{ $row->minute }}分</td>
+                            <td>{{ $row->fee }}円</td>
+                            <td>{{ $row->form }}</td>
+                            <td>
+                                <form enctype="multipart/form-data" action="{{ url('/works/payslip/' . $row->id) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <button type="submit" class="btn btn-danger">削除</button>
+                                </form>
+                            </td>
+                        @endforeach
+                    </tr>
+                @else
+                    <p>該当するデータが見つかりませんでした。</p>
+                @endif
+            </table>
+            <h2>合計</h2>
+            <table class="table bg-white">
                 <tr>
                     <th>合計出勤日数</th>
                     <th>合計出勤時間</th>
@@ -26,34 +55,11 @@
                     <th>合計金額</th>
                 </tr>
                 <tr>
+                    {{--TODO: 見直す必要あり--}}
                     <td>20日</td>
                     <td>{{ $sumHour }}時間{{ $sumMinute }}分</td>
                     <td>{{ $sumFee }}円</td>
                     <td>{{ $sumHour + $sumMinute * 1000 }}円</td>
-                </tr>
-            </table>
-            <table class="table bg-white">
-                <tr>
-                    <th>月日</th>
-                    <th>出勤時間</th>
-                    <th>交通費</th>
-                    <th>出勤形態</th>
-                    <th>&nbsp;</th>
-                </tr>
-                <tr>
-                    @foreach($payslip as $row)
-                        <td>{{ $row->date }}</td>
-                        <td>{{ $row->hour }}時間{{ $row->minute }}分</td>
-                        <td>{{ $row->fee }}円</td>
-                        <td>{{ $row->form }}</td>
-                        <td>
-                            <form enctype="multipart/form-data" action="{{ url('/works/payslip/' . $row->id) }}" method="POST">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
-                                <button type="submit" class="btn btn-danger">削除</button>
-                            </form>
-                        </td>
-                    @endforeach
                 </tr>
             </table>
             <div class="text-center">
