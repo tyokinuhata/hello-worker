7a56b2b350b93e2182403d2260b76a670ff84ce7
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-06 23:25:44 +0900

    出勤時間格納マイグレーションの作成

---
+++ database/migrations/2018_02_06_141534_create_times_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('times', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->date('date')->comment('出勤日');
+            $table->unsignedInteger('hour');
+            $table->unsignedInteger('minute');
+            $table->unsignedInteger('fee')->comment('交通費');
+            $table->unsignedInteger('form')->comment('出勤形態');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('times');
+    }
+}
